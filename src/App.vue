400f12077b94b11cccc9c329b41ad7b21f860f6c
LeonardJ <unmatchedhide@gmail.com>
2026-07-29 13:14:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <router-view />
+    <v-snackbar-queue v-model="snackbar.queue" />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { useSnackbarStore } from './stores/snackbar'
+
+  const snackbar = useSnackbarStore()
+</script>
